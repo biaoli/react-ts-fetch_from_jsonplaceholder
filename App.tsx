e53699058dfd26f5e5aff3cadd0d9fc397ff4f9f
biaoli <biaoli6@hotmail.com>
2022-12-28 12:28:09 -0500

    remove redundant from App.tsx and modify sytle.css

--- App.tsx
+++ App.tsx
@@ -1,33 +1,29 @@
-//import * as React from 'react';
-import React, { useState, useEffect } from "react";
-import ReactDOM from "react-dom";
+import React, { useState, useEffect } from 'react';
 
 import './style.css';
 
 export default function App() {
-
   const [posts, setPosts] = useState([]);
 
-  const url = "https://jsonplaceholder.typicode.com/posts";
+  const url = 'https://jsonplaceholder.typicode.com/posts';
 
   useEffect(() => {
     fetch(url)
-      .then(res => {
+      .then((res) => {
         if (!res.ok) {
-          return Error("Oh no");
+          return Error('Oh no');
         }
         return res.json();
       })
-      .then(data => setPosts(data));
+      .then((data) => setPosts(data));
   });
 
   return (
-    <div className="App">
-      <h1>Posts</h1>
-      <h2>...are here</h2>
-
-      {posts.map(post => (
-        <div key={post.id} className="post-block">{post.title}</div>
+    <div>
+      {posts.map((post) => (
+        <div key={post.id} className="post-block">
+          {post.title}
+        </div>
       ))}
     </div>
   );
